dc9e78fac1e8f19564f5b2b49b97a517391ff5e4
chadness12 <43721867+chadness12@users.noreply.github.com>
2022-11-16 16:46:10 +0900

    Update settings.py

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -83,7 +83,10 @@ DATABASES = {
         'PORT': "3306"
     }
 }
-
+''' 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    } '''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
