b2e2f4f5c40fec598c221dab5788c18e52c77c7e
chadness12 <43721867+chadness12@users.noreply.github.com>
2022-11-16 14:39:23 +0900

    change Dockerfile && settings.py

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -84,7 +84,10 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USERNAME'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT')
+        'PORT': os.getenv('DATABASE_PORT'),
+        'OPTIONS': {
+            'ssl': {'ssl-ca': './BaltimoreCyberTrustRoot.crt.pem'}
+        }
     }
 }
 
