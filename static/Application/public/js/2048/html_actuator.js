b1df01418b768714ed72dc15c147df6e9ddc9d5a
chadness12 <43721867+chadness12@users.noreply.github.com>
2022-11-18 16:25:40 +0900

    test

--- static/Application/public/js/2048/html_actuator.js
+++ static/Application/public/js/2048/html_actuator.js
@@ -133,10 +133,10 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 HTMLActuator.prototype.message = function (won) {
   
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Go Ranking";
   
   this.messageContainer.classList.add(type);
-  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+  this.messageContainer.getElementsByTagName("a")[0].textContent = message;
 };
 
 HTMLActuator.prototype.clearMessage = function () {
