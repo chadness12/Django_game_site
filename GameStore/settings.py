7ca6a49bd4734a9cd07b02f3e974f1f994463a49
chadness12 <43721867+chadness12@users.noreply.github.com>
2022-11-28 13:30:23 +0900

    Update settings.py

--- GameStore/settings.py
+++ GameStore/settings.py
@@ -75,19 +75,22 @@ WSGI_APPLICATION = 'GameStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.mysql",
-        'NAME': "gamedb",
-        'USER': "testroot@dbtest-01",
-        'PASSWORD': "dkagh123.",
-        'HOST': "dbtest-01.mysql.database.azure.com",
-        'PORT': "3306"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 ''' 'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     } '''
-
+''' 'default': {
+        'ENGINE': "django.db.backends.mysql",
+        'NAME': "gamedb",
+        'USER': "testroot@dbtest-01",
+        'PASSWORD': "dkagh123.",
+        'HOST': "dbtest-01.mysql.database.azure.com",
+        'PORT': "3306"
+    } '''
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
